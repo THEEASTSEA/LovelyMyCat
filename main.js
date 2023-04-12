99c11e91350c2f133b14ff760bb32730a11a00c5
donghae <todonghae@gmail.com>
2023-04-12 14:50:26 +0900

    사랑스러운 나의 고양이

---
+++ main.js
@@ -0,0 +1,35 @@
+
+async function dongWithFetch() {
+  const dog = await fetch("https://api.thecatapi.com/v1/images/search", {
+    method: "GET",
+    headers: {
+      "conteent-type": "application/json",
+      "x-api-key": "live_ OILgWBDOdSSaiZrjLCZNgmoODOHjTT n7jKf7HYmEWKkKxgZtl0i1G4ZJDKvB 9Jt9",
+    },
+  });
+  const json = await dog.json()
+  const imageUrl = json[0].url
+  const image = document.createElement('img')
+  image.src = imageUrl
+  document.querySelector(".list").appendChild(image)
+}
+
+for (let i = 0; i < 12; i++) {
+  dongWithFetch()
+}
+
+// 무한 스크롤
+// window.innerHeight : 브라우저 영역의 높이
+// window.scrollY : 스크롤의 이동 높이
+// document.body.offsetHeight : 요소의 실제 높이(보이는 영역 + 스크롤 영역)
+
+window.addEventListener('scroll', () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    // const toAdd = document.createElement('img');
+    // toAdd.src = image
+    // document.querySelector(".list").appendChild(toAdd);
+    for (let i = 0; i < 9; i++) {
+      dongWithFetch()
+    }
+  }
+});
